9843cc756da15c2809efb1640d6ec38b0bcee518
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-09-17 10:09:08 -0400

    Backend configuration

--- backend/src/main/java/com/capstone/backend/BackendApplication.java
+++ backend/src/main/java/com/capstone/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.catcards.backend;
+package com.capstone.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/capstone/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/capstone/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.catcards.backend;
+package com.capstone.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
