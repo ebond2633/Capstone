f44d80d73967ec8e8192210f38183027c5afccbc
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-09-16 20:53:58 -0400

    Added Home Page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+     <h1 className="text-3xl font-bold underline text-center bg-pink-500">
+    Hello world to Verdant Elegance!
+  </h1>
+  )
+}
+
+export default Home;
